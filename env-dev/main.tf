2046faa638074e6d548e7ea0386b356fe23f913e
Brendan ODonnell <brendan.james.odonnell@gmail.com>
2016-12-14 18:42:33 -0600

    Add chef-clients to instances

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -4,6 +4,8 @@ variable "region"       { }
 variable "azs"          { type = "list" }
 variable "vpc_cidr"     { }
 variable "public_cidrs" { type = "list" }
+variable "chef_org"     { }
+variable "chef_validation_file" { }
 
 variable "public_key"   { default = "~/.ssh/id_rsa.pub" }
 
@@ -32,24 +34,31 @@ resource "aws_ecs_cluster" "default" {
 module "bastion" {
   source = "../modules/instance"
 
+  env = "${var.env_name}"
   domain = "${var.domain}"
   role = "bastion"
   vpc_id = "${module.vpc.id}"
   subnet_ids = "${module.vpc.public_subnet_ids}"
   key_name = "${aws_key_pair.deploy.key_name}"
 
+  chef_org = "${var.chef_org}"
+  chef_validation_file = "${var.chef_validation_file}"
   ingress_ports = ["tcp/22"]
 }
 
 module "ecs" {
   source = "../modules/instance"
 
+  env = "${var.env_name}"
   domain = "${var.domain}"
   role = "ecs"
   vpc_id = "${module.vpc.id}"
   subnet_ids = "${module.vpc.public_subnet_ids}"
   key_name = "${aws_key_pair.deploy.key_name}"
 
+  chef_org = "${var.chef_org}"
+  chef_validation_file = "${var.chef_validation_file}"
+  chef_bastion_host = "${module.bastion.instance_ids[0]}"
   desired_capacity = 3
   iam_profile = "ecs"
   user_data_file = "ecs.yml"
